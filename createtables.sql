a08a92626013cbefffa1280561496dc4a1eb8b3b
0bryan0 <bryang3652@gmail.com>
2022-08-19 15:58:21 -0500

    tables created

--- createtables.sql
+++ createtables.sql
@@ -1,120 +1,109 @@
-USE dormdimensions;
 DROP TABLE IF EXISTS burtonInfo;
 CREATE TABLE burtonInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS cassatInfo;
 CREATE TABLE cassatInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS davisInfo;
 CREATE TABLE davisInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS evansInfo;
 CREATE TABLE evansInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS goodhueInfo;
 CREATE TABLE goodhueInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS jamesInfo;
 CREATE TABLE jamesInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS musserInfo;
 CREATE TABLE musserInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS myersInfo;
 CREATE TABLE myersInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS nourseInfo;
 CREATE TABLE nourseInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS severenceInfo;
 CREATE TABLE severenceInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
 
-USE dormdimensions;
 DROP TABLE IF EXISTS watsonInfo;
 CREATE TABLE watsonInfo (
-  Building, text
-  Room text
-  RoomType text
-  Dimensions text
-  Area int
+  Building text,
+  Room text,
+  RoomType text,
+  Dimensions text,
+  Area int,
   AdjoiningRooms text
 );
